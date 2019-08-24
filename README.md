176c8666c21b6e240f044ee3b43fbf20b7c90eec
perceptron007 <dprophecyguy@gmail.com>
2019-08-24 11:33:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# functional-programming-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/functional-programming-1)
---
+++ index.js
@@ -0,0 +1,19 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('rainbow');
+
+const addColor = color => {
+  const element = document.getElementById('rainbow');
+  const div = document.createElement('div');
+  div.style.paddingTop = '10px';
+  div.style.backgroundColor = color;
+  element.appendChild(div);
+}
+
+addColor('red');
+addColor('orange');
+addColor('yellow');
+addColor('green');
+addColor('aqua');
