42c8649829ff3e0c6728afca1e94872296c87f59
perceptron007 <dprophecyguy@gmail.com>
2019-08-24 14:04:19 +0530

    Changed call into functional Call

--- index.js
+++ index.js
@@ -2,20 +2,24 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('rainbow');
-
-const addColor = color => {
-  const element = document.getElementById('rainbow');
+const getColorElement = (color) => {
   const div = document.createElement('div');
   div.style.paddingTop = '10px';
   div.style.backgroundColor = color;
-  element.appendChild(div);
+  div.style.paddingBottom = '10px';
+  return div;
 }
 
 const colors = ['red', 'aqua', 'green', 'yellow', 'blue'];
-
+const ids = ['rainbow', 'cycle'];
 // What if the addColor was supposed to return a value, forEach will ignore it ?
 // What can we do to solve this problem ?
 // Change the function above so that it accepts an array of different ids and apply the same logic all over them
+const getElementsById = id => document.getElementById(id);
+const elements = ids.map(getElementsById);
 
-colors.forEach(addColor);
+const appendChild = (colorElement) => (parentElement) => parentElement.appendChild(colorElement);
+const colorElements = colors.map(getColorElement);
+const colors= colorElements.map(appendChild);
+
+elements.forEach(item => colors.forEach(f => f(item)));
