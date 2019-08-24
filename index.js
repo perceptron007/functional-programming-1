9e37a122ac85eed4df0145b0284ea527e5e6ad89
perceptron007 <dprophecyguy@gmail.com>
2019-08-24 15:08:35 +0530

    Final Solution

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Import stylesheets
 import './style.css';
 
+
+const colors = ['red', 'aqua', 'green', 'yellow', 'blue'];
+const ids = ['rainbow', 'cycle'];
+
 // Write Javascript code!
 const getColorElement = (color) => {
   const div = document.createElement('div');
@@ -10,15 +14,15 @@ const getColorElement = (color) => {
   return div;
 }
 
-const colors = ['red', 'aqua', 'green', 'yellow', 'blue'];
-const ids = ['rainbow', 'cycle'];
-// What if the addColor was supposed to return a value, forEach will ignore it ?
-// What can we do to solve this problem ?
-// Change the function above so that it accepts an array of different ids and apply the same logic all over them
 const getElementsById = id => document.getElementById(id);
-const appendChild = (colorElement) => (parentElement) => parentElement.appendChild(colorElement);
 
-const colorsFunction= colors.map(getColorElement).map(appendChild);
+const appendChild = (colorElement) => (parentElement) =>      parentElement.appendChild(colorElement);
+
+const colorsFunction = 
+  colors.map(getColorElement)
+        .map(appendChild);
+
+const applyColor = element => colorsFunction.map(f => f(element));
 
 ids.map(getElementsById)
-   .map(item => colorsFunction.forEach(f => {f(item); console.log(item)}));
+   .map(applyColor);
